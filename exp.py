4163cfd66a568e1d9b1518d96003eb9e1385e14d
Vinay Pillai <vinaypillai01@gmail.com>
2020-11-02 14:37:03 -0800

    add dns queries

--- exp.py
+++ exp.py
@@ -2,17 +2,31 @@
 
 import requests
 import json
+import dns.resolver
+import tldextract
+
 results = []
 with open("blocked.txt") as infile, open("results.jsonl","w") as outfile:
     for row in infile:
         site = row.strip()
-        data = {'site':site}
+        data = {'site':site, 'http':{}, 'dns':{}}
+        # Perform http request to blocked url
         try:
             r = requests.get(site)
-            data['status'] = r.status_code;
-            data['content'] = r.text
+            data['http']['status'] = r.status_code;
+            data['http']['content'] = r.text
         except requests.exceptions.RequestException as err:
-            data['error'] = str(err)
+            data['http']['error'] = str(err)
+
+        # Perform dns request to blocked url
+        try:   
+            extracted_site = tldextract.extract(site)
+            hostname = '.'.join([part for part in list(extracted_site) if len(part) > 0])
+            response = dns.resolver.resolve(hostname, 'A').response
+            data['dns']['rcode'] = response.rcode()
+            data['dns']['content'] = str(response)
+        except dns.exception.DNSException as err:
+            data['dns']['error'] = err.msg
         results.append(data)
 
     for row in results:

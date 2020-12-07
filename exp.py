777a9f67595eac2171e8c9f48110e81066b28242
Vinay Pillai <vinaypillai01@gmail.com>
2020-12-07 23:29:58 +0000

    add debug messages

--- exp.py
+++ exp.py
@@ -4,11 +4,30 @@ import requests
 import json
 import dns.resolver
 import tldextract
+import os
+from sys import stderr
+import time
 
 results = []
-with open("blocked.txt") as infile, open("results.jsonl","w") as outfile:
+timestamp = int(time.time())
+print("---", file=stderr)
+print("Starting experiment...", file=stderr)
+print(f"TIME:{timestamp}", file=stderr)
+print("---", file=stderr)
+outfile_name = f"results-{timestamp}.jsonl"
+file_count = 0
+while(os.path.exists(outfile_name)):
+    file_count += 1
+    outfile_name = f"results-{timestamp}_{file_count}.jsonl"
+
+with open("blocked.txt") as infile, open(outfile_name,"w") as outfile:
+    os.chmod(outfile_name, 0o666)
     for row in infile:
         site = row.strip()
+        print("---", file=stderr)
+        print("Starting new site...", file=stderr)
+        print(f"SITE:{site}", file=stderr)
+        print("---", file=stderr)
         data = {'site':site, 'http':{}, 'dns':{}}
         # Perform http request to blocked url
         try:
@@ -27,7 +46,11 @@ with open("blocked.txt") as infile, open("results.jsonl","w") as outfile:
             data['dns']['content'] = str(response)
         except dns.exception.DNSException as err:
             data['dns']['error'] = err.msg
-        results.append(data)
-
-    for row in results:
-        outfile.write(f"{json.dumps(row)}\n")
+        outfile.write(f"{json.dumps(data)}\n")
+        outfile.flush()
+        os.fsync(outfile)
+finish_timestamp = int(time.time())
+print("Finishing experiment...", file=stderr)
+print(f"TIME:{finish_timestamp}", file=stderr)
+print(f"DURATION:{finish_timestamp - timestamp}", file=stderr)
+print("---", file=stderr)
